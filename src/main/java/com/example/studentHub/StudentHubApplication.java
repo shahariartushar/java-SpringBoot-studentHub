60f447a7970f63ed44f540107be643f99a982716
shahariartushar <shahariartushar@gmail.com>
2023-02-17 20:56:52 +0100

    Initial commit

---
+++ src/main/java/com/example/studentHub/StudentHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/studentHub/StudentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
